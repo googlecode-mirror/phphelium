fbe29566418ac8afc310099f482135e36bdeb160
bryan@bryanhealey.com <bryan@bryanhealey.com@e9a7245f-7974-5460-7f2c-9ca00917ebd9>
2012-03-14 15:48:12 +0000

    Important changes / packaging

--- trunk/scripts/buildModel.php
+++ trunk/scripts/buildModel.php
@@ -8,16 +8,7 @@
  *          can only be run from the command line
  */
 
-$ini = parse_ini_file('../settings.ini',true);
-define('ROOT',$ini['system']['root']);
-define('SRC',$ini['system']['src']);
-define('MASTER_DB_STRING',$ini['database']['master']);
-define('SLAVE_DB_STRING',$ini['database']['slave']);
-define('MEMCACHE_SERVERS',$ini['memcache']['serverList']);
-define('MEMCACHE_DEFAULT_EXPIRY',$ini['memcache']['defaultExpiry']);
-
-require_once('../src/core/DB.php');
-require_once('../src/core/Model.php');
+require_once('../src/prepare.php');
 
 $model = $argv[1];
 require_once('../src/models/'.$model.'.php');
