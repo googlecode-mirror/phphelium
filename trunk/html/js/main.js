a158cc2cde6d4decd0b87fa84ad33c3acfa982dd
bryan@bryanhealey.com <bryan@bryanhealey.com@e9a7245f-7974-5460-7f2c-9ca00917ebd9>
2012-06-15 16:29:39 +0000

    v2.0 - major changes

---
+++ trunk/html/js/main.js
@@ -0,0 +1,331 @@
+/*
+ * main.js
+ * Copyright: Bryan Healey 2010, 2011, 2012 (bryan@bryanhealey.com)
+ * License: GNU General Public License (v3)
+ * Purpose: All collected base functionality
+ */
+
+__CARRIAGE = 13;
+
+var sys = {
+    openTime: false,
+
+    init: function() {
+        Hasher.init();
+        $('#popup-container').draggable();
+        sys.openTime = DateData.unixtime();
+    },
+
+    toggleLanguage: function(language) {
+        ajax.onSuccess = function(e) {
+            if (e['result'] == 'success') window.location = window.location.pathname;
+            else alert('An error has occured');
+        }
+
+        ajax.request('/update-language/','POST',{'language':language});
+    },
+    
+    getBrowser: function() {
+        var browser = "unknown";
+        var agent = navigator.userAgent.toLowerCase();
+        var possibleMatches = new Array('safari','msie 6','msie','firefox','netscape','omniweb','avantbrowser','msn','konqueror','camino','chrome');
+
+        for (var i=0;i<possibleMatches.length;i++) {
+            if (agent.indexOf(possibleMatches[i]) > -1) {
+                browser = possibleMatches[i];
+                break;
+            }
+        }
+
+        return browser;
+    }
+};
+
+var pos = {
+    rightOf: function(pnode,obj) {
+        var off = $(pnode).offset();
+        $(obj).offset({ 'top': (off.top+1), 'left': (off.left+$(pnode).width()+20) });
+    }
+};
+
+var navAssist = {
+    navRegister: {},
+    prepareNav: function(navId,defVal) {
+        navAssist.navRegister[navId] = {};
+        navAssist.navRegister[navId]['active'] = defVal;
+
+        Hasher.init();
+        if (Hasher.hash['tab']) navAssist.navRegister[navId]['active'] = Hasher.hash['tab'];
+
+        $('#'+navId).children().each(function(){
+            var kid = $(this).attr("id");
+            var title = $(this).attr("makeTitle");
+            var base = kid.replace("t","");
+
+            if (base == Hasher.hash['tab']) {
+                $('#t'+base).addClass('active');
+                $('#c'+base).css("display","block");
+
+                if (title !== undefined) document.title = title;
+                navAssist.navRegister[navId]['active'] = base;
+            } else {
+                $('#t'+base).removeClass('active');
+                $('#c'+base).css("display","none");
+            }
+
+            $('#t'+base).click(function() {
+                if (title !== undefined) document.title = title;
+                Hasher.set('tab',base);
+                $('#t'+navAssist.navRegister[navId]['active']).removeClass('active');
+                $('#c'+navAssist.navRegister[navId]['active']).css("display","none");
+
+                $('#t'+base).addClass('active');
+                $('#c'+base).css("display","block");
+
+                navAssist.navRegister[navId]['active'] = base;
+            });
+        });
+
+        $('#t'+navAssist.navRegister[navId]['active']).addClass('active');
+        $('#c'+navAssist.navRegister[navId]['active']).css("display","block");
+    }
+};
+
+var formAssist = {
+    clear: function(formId) {
+        var el = document.getElementById(formId);
+
+        for(var i in el.elements) {
+            switch(el.elements[i].type) {
+                case "text": el.elements[i].value = ""; break;
+                case "checkbox": el.elements[i].checked = false; break;
+                case "radio": el.elements[i].checked = false; break;
+                case "password": el.elements[i].value = ""; break;
+            }
+        }
+    },
+
+    disable: function(formId) {
+        var el = document.getElementById(formId);
+
+        for(var i in el.elements) {
+            el.elements[i].disabled = true;
+        }
+    },
+
+    enable: function(formId) {
+        var el = document.getElementById(formId);
+
+        for(var i in el.elements) {
+            el.elements[i].disabled = false;
+        }
+    },
+
+    clearList: function(list) {
+        for (var i in list) {
+            $('#'+list[i]).removeAttr("checked");
+        }
+    }
+};
+
+var popup = {
+    closer: null,
+    loader: null,
+
+    load: function(top,title) {
+        $('#popup-container').css('visibility','hidden');
+        $('#popup-container').css('display','block');
+
+        if (title) {
+            $('#popup-title').css('display','block');
+            $('#popup-title').html(title);
+        }
+
+        popup.correct();
+
+        if (navigator.userAgent.toLowerCase().indexOf("msie") > -1) {
+            $('#popup-container').css('visibility','visible');
+            $('#popup-container').css('opacity',1);
+        } else {
+            $('#popup-container').css('opacity',0);
+            $('#popup-container').css('visibility','visible');
+            $('#popup-container').animate({
+                opacity: 1
+            },200);
+        }
+    },
+
+    update: function(content) {
+        $('#popup-content').html(content);
+        popup.correct();
+    },
+
+    trigger: function(title,uri,method,appendParams) {
+        if (!method) var method = 'GET';
+        if (!appendParams) var appendParams = {};
+
+        var params = {};
+        params['params'] = appendParams;
+        params['title'] = title;
+        params['uri'] = uri;
+        params['method'] = method;
+        
+        popup.prepare(params);
+    },
+
+    raw: function(title,data,method,appendParams) {
+        if (!method) var method = 'GET';
+        if (!appendParams) var appendParams = {};
+
+        var params = {};
+        params['params'] = appendParams;
+        params['title'] = title;
+        params['raw'] = data;
+        params['method'] = method;
+
+        popup.prepare(params);
+    },
+    
+    prepare: function(params) {
+        if ($('#popup-container').css('display') == 'none') {
+            popup.process(params);
+        } else {
+            $('#popup-container').animate({
+                opacity: 0
+            },200,function() {
+                popup.process(params);
+            });
+        }
+    },
+
+    process: function(params) {
+        if (params.uri || params.raw) {
+            if (!params.params) params.params = {};
+            if (!params.method) params.method = 'GET';
+
+            if (params.top) params.top = "'"+params.top+"'";
+            else params.top = false;
+
+            if (params.uri) {
+                ajax.onSuccess = function() {
+                    setTimeout(function() {
+                        popup.load(params.top,params.title);
+                    },200);
+
+                    if (popup.loader) popup.loader();
+                }
+
+                ajax.request(params.uri,params.method,params.params,'popup-content');
+            } else if (params.raw) {
+                $('#popup-content').html(params.raw);
+
+                setTimeout(function() {
+                    popup.load(params.top,params.title);
+                },200);
+            }
+        }
+    },
+
+    hide: function(bypassCloser) {
+        if (!bypassCloser) var bypassCloser = false;
+
+        var allowClose = true;
+        if (!bypassCloser && popup.closer && !popup.closer()) allowClose = false;
+
+        if (allowClose) {
+            $('#popup-container').animate({
+                opacity: 0
+            },200);
+
+            setTimeout(function() {
+                $('#popup-container').css('display','none');
+            },200);
+        } else popup.closer();
+    },
+
+    correct: function() {
+        $('#popup-container').center();
+    }
+};
+
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px");
+    this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
+    return this;
+}
+
+String.prototype.isDate = function() {
+    var txt = this;
+    if (txt.length > 10 && txt.substr(10,1) == " ") txt = txt.substr(0,10);
+
+    var objDate, mSeconds;
+
+    if (txt.length != 10) return false;
+
+    var year = txt.substr(0,4);
+    var month = txt.substr(5,2);
+    var day = txt.substr(8,2);
+
+    if (txt.substr(4,1) != '/') return false;
+    if (txt.substr(7,1) != '/') return false;
+
+    if (year < 999 || year > 3000) return false;
+
+    mSeconds = (new Date(year,month,day)).getTime();
+
+    objDate = new Date();
+    objDate.setTime(mSeconds);
+
+    if (objDate.getFullYear() != year) return false;
+    if (objDate.getMonth() != month) return false;
+    if (objDate.getDate() != day) return false;
+
+    return mSeconds;
+};
+
+String.prototype.strripos = function(needle, offset) {
+    haystack = (this+'').toLowerCase();
+    needle = (needle+'').toLowerCase();
+
+    var i = -1;
+    if (offset) {
+        i = (haystack+'').slice(offset).lastIndexOf(needle); // strrpos' offset indicates starting point of range till end,
+        // while lastIndexOf's optional 2nd argument indicates ending point of range from the beginning
+        if (i !== -1) {
+            i += offset;
+        }
+    }
+    else {
+        i = (haystack+'').lastIndexOf(needle);
+    }
+    return i >= 0 ? i : false;
+};
+
+function strip_tags(input,allowed) {
+    allowed = (((allowed || "") + "").toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join(''); // making sure the allowed arg is a string containing only tags in lowercase (<a><b><c>)
+    var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi,
+        commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
+
+    return input.replace(commentsAndPhpTags, '').replace(tags, function($0, $1) {
+        return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
+    });
+}
+
+function replaceIt(input,search,sub) {
+    while(input.indexOf(search) > -1) input = input.replace(search,sub);
+    return input;
+}
+
+function addCommas(str) {
+    var amount = new String(str);
+    amount = amount.split("").reverse();
+
+    var output = "";
+    for (var i = 0; i <= amount.length-1; i++) {
+        output = amount[i] + output;
+        if ((i+1) % 3 == 0 && (amount.length-1) !== i) output = ',' + output;
+    }
+
+    return output;
+}
---
+++ trunk/system/core/Language.php
@@ -0,0 +1,132 @@
+<?php
+namespace Helium;
+
+/*
+ * Language.php
+ * Copyright: Bryan Healey 2010, 2011 (bryan@bryanhealey.com)
+ * License: GNU General Public License (v3)
+ * Purpose: Process language actions
+ */
+
+class Language {
+    private $language;
+
+    function __construct($lang=false) {
+        if (empty($lang)) $lang = Session::getLanguage();
+        $this->language = $lang;
+    }
+
+    /**
+     *
+     * function: getLanguage
+     * Get the active language
+     * @access public
+     * @return string
+     */
+    public function getLanguage() {
+        return $this->language;
+    }
+
+    /**
+     *
+     * function: loadLanguage
+     * Load a language file
+     * @access public
+     * @param string $ltmp
+     * @param string $lang (optional)
+     * @return string
+     */
+    public function loadLanguage($ltmps=array('GLOBAL'),$lang=false,$preload=false) {
+        if (!is_array($ltmps)) $ltmps = array($ltmps);
+        $root = str_replace('system/core','',realpath(dirname(__FILE__)));
+        
+        if (empty($lang)) $lang = Session::getLanguage();
+
+        $langList = array();
+        foreach($ltmps as $ltmp) {
+            if (substr_count($ltmp,'.tmp')) $ltmp = str_replace('.tmp','',$ltmp);
+
+            $cache = Cache::init();
+            $tmpList = $cache->get('store[environment[language]['.$ltmp.'-'.$lang.']');
+            if (empty($tmpList)) {
+                $langFile = Language::loadLanguageFile($lang);
+                if (!empty($langFile) && !empty($langFile[$ltmp])) {
+                    $langList = array_merge($langList,$langFile[$ltmp]);
+                    $cache->set('store[environment[language]['.$ltmp.'-'.$lang.']',serialize($langFile[$ltmp]));
+                }
+            } else $langList = array_merge($langList,unserialize($tmpList));
+        }
+        
+        if ($preload === true) {
+            if (!empty($langList)) {
+                $tmp = Templater::init();
+                foreach($langList as $pid => $phrase) {
+                    $tmp->setGlobal('LANG['.$pid.']',$phrase);
+                }
+            }
+        } else return $langList;
+    }
+
+    public function loadLanguageFile($lang=false) {
+        $langList = array();
+        
+        if (empty($lang)) $lang = Session::getLanguage();
+
+        $root = str_replace('system/core','',realpath(dirname(__FILE__)));
+        if (!empty($_SERVER['SERVER_NAME']) && file_exists($root.'src/custom/'.$_SERVER['SERVER_NAME'].'/language/'.$lang.'.xml')) $langFile = $root.'src/custom/'.$_SERVER['SERVER_NAME'].'/language/'.$lang.'.xml';
+        else $langFile = $root.'language/'.$lang.'.xml';
+        
+        $cache = Cache::init();
+        $langList = $cache->get('store[environment[languageFile]['.md5($langFile).']');
+        if (!empty($langList)) return unserialize($langList);
+        
+        if (file_exists($langFile)) $xml = (array)simplexml_load_file($langFile);
+        if (!empty($xml)) {
+            $langBlocks = array();
+            foreach($xml['tmp'] as $t) {
+                $opt = (string)$t['name'];
+                $langBlocks[$opt] = $t->phrases;
+            }
+            
+            if (!empty($langBlocks)) {
+                foreach($langBlocks as $block => $langBlock) {
+                    $langBlock = (array)$langBlock;                    
+                    if (is_array($langBlock['phrase'])) {
+                        foreach($langBlock['phrase'] as $phrase) {
+                            $phrase = (array)$phrase;
+                            if (!empty($phrase['id'])) $langList[$block][$phrase['id']] = trim((string)$phrase['sub']);
+                        }
+                    } else {
+                        $langBlock = (array)$langBlock['phrase'];
+                        $langList[$block][$langBlock['id']] = trim((string)$langBlock['sub']);
+                    }
+                }
+            }
+
+            unset($langBlocks);
+        }
+        
+        $cache->set('store[environment[languageFile]['.md5($langFile).']',serialize($langList));
+        return $langList;
+    }
+
+    public function getSub($id,$ltmp='GLOBAL',$lang=false) {
+        $language = Language::loadLanguageFile($lang);
+        return $language[$ltmp][$id];
+    }
+
+    public function available() {
+        $root = str_replace('system/core','',realpath(dirname(__FILE__)));
+
+        $packs = array();
+        if ($handle = opendir($root.'language')) {
+            while (false !== ($entry = readdir($handle))) {
+                if (!in_array($entry,array('.','..'))) {
+                    $packs[] = $entry;
+                }
+            }
+        }
+
+        return $packs;
+    }
+}
